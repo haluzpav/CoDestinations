7799ab46f2406d1be7d1036f3fa0d34a61dc440c
Pavel Haluza <24211726+haluzpav@users.noreply.github.com>
2022-09-27 12:11:23 +0200

    basic destinations

--- codestinations/src/main/kotlin/cz/applifting/codestinations/BasicScreen.kt
+++ codestinations/src/main/kotlin/cz/applifting/codestinations/BasicScreen.kt
@@ -1,10 +1,37 @@
 package cz.applifting.codestinations
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.NavGraph
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.navigate
+import cz.applifting.codestinations.destinations.BasicSubScreenDestination
 
+@RootNavGraph // sets BasicNavGraph as a nested nav graph of RootNavGraph
+@NavGraph // marks BasicNavGraph as a NavGraph annotation
+annotation class BasicNavGraph(
+    val start: Boolean = false,
+)
+
+@BasicNavGraph(start = true)
 @Destination
 @Composable
-fun BasicScreen() {
+fun BasicScreen(navController: NavController) {
+    Column {
+        Text("Basic screen")
+        Button(onClick = { navController.navigate(BasicSubScreenDestination) }) {
+            Text("Go to sub-screen")
+        }
+    }
+}
 
+@BasicNavGraph
+@Destination
+@Composable
+fun BasicSubScreen() {
+    Text("Basic sub-screen 1")
 }
